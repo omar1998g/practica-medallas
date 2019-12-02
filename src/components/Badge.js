5cf79a46191489de8c7b0c43dc20deac7b11b451
Omar González <omar.1998g@gmail.com>
2019-12-02 01:29:55 +0100

    Estilos aplicados

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,23 +2,24 @@ import React from 'react';
 
 import confLogo from '../images/badge-header.svg';
 // import avatar from '../images';
+import "./styles/Badge.css";
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Logo" />
                 </div>
-                <div>
-                    <img src="" alt="Avatar" />
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://i.pinimg.com/originals/dd/84/dd/dd84ddcd127174dda78db0b01995a6b9.jpg" alt="Avatar" />
                     <h1>Omar <br /> González</h1>
                 </div>
-                <div>
-                    <p>Frontend Engineer</p>
-                    <p>@omarperoelbueno</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Engineer</h3>
+                    <div>@omarperoelbueno</div>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/Badge'
+import Badge from './components/Badge';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
 // const element = <h1>Hola holholaaaaa</h1>;
 
